7f6a079216f3b35ce5cb698c69eed49195f6f601
taalaibekov28 <nuriktaalajbekov8@gmail.com>
2021-01-21 22:49:13 +0600

    commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+x = 10 ; y = 112
+print("x = ",x, "; y = ", y, sep="")
+
+x, y = y, x
+print("x = ",x, "; y = ", y, sep="")
+
+
+
+
+get init
+git add .
+git commit -m "first commit"
+
+git remote add origin https://github.com/taalaibekov28/nurik.git
+git push -u origin masterbra
+
+
+git remote add origin https"//github.com/taalaibekov28/nurik.git
+git branch -M main
+git push -u origin mainmai
